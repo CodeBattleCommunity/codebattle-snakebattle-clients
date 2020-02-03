230075b06f890a0822aad8d981511dad30aabbbf
Anton_Nikitin <anton_nikitin@epam.com>
2020-02-03 19:09:44 +0400

    python client

---
+++ CodeBattlePython/snakebattleclient/SnakeBattleClient.py
@@ -0,0 +1,49 @@
+import logging
+import websocket
+
+from snakebattleclient.internals.Board import Board
+from snakebattleclient.internals.SnakeAction import SnakeAction
+
+logger = logging.getLogger(__name__)
+
+
+def sample(ws):
+    pass
+
+
+class GameClient:
+    def __init__(self, url):
+        path = url.replace("http", "ws")
+        path = path.replace("board/player/", "ws?user=")
+        path = path.replace("?code=", "&code=")
+
+        logger.info("connecting... {}".format(path))
+        self.socket = websocket.WebSocketApp(path,
+                                             on_message=lambda ws, msg: self.on_message(ws, msg),
+                                             on_error=lambda ws, err: self.on_error(ws, err),
+                                             on_close=lambda ws: self.on_close(ws),
+                                             on_open=lambda ws: self.on_open(ws))
+
+    def run(self, on_turn=lambda a: SnakeAction.STOP):
+        self.on_turn = on_turn
+        self.socket.run_forever()
+
+    def on_message(self, ws, message):
+        board = Board(message.lstrip("board="))
+        board.print_board()
+        action = self.on_turn(board)
+        self.__send(action)
+        # .value)
+
+    def __send(self, msg):
+        logger.info('Sending: {}'.format(msg))
+        self.socket.send(msg)
+
+    def on_open(self, ws):
+        logger.info('Connection established: {}'.format(ws))
+
+    def on_error(self, ws, error):
+        logger.error(error)
+
+    def on_close(self, ws):
+        logger.info("### disconnected ###")
---
+++ CodeBattlePython/snakebattleclient/internals/SnakeAction.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class SnakeAction(Enum):
+    LEFT = 'left'
+    RIGHT = 'right'
+    UP = 'up'
+    DOWN = 'down'
+    STOP = 'stop'
+    ACT_LEFT = 'act,left'
+    ACT_RIGHT = 'act,right'
+    ACT_UP = 'act,up'
+    ACT_DOWN = 'act,down'
+    ACT_STOP = 'act,stop'
