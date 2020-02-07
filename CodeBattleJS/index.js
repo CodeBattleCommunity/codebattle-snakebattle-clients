ac66165a4175f9dd9e417438482cb51936132e42
Maksim Samovolkin <samovolkinmaxim.null@gmail.com>
2020-02-07 18:52:58 +0400

    remove test server url

--- CodeBattleJS/index.js
+++ CodeBattleJS/index.js
@@ -1,6 +1,5 @@
 const outputElement = document.getElementById("text");
-const url =
-  "http://epruizhsa0001t2:8080/codenjoy-contest/board/player/1g1z6yb04udfp18d6hms?code=2897168687142116884&gameName=snakebattle";
+const url = "insert link from your browser here"
 
 const client = new GameClient(url, {
   onUpdate: board => {
