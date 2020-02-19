cddaf040dc5070c893d42319f429c4ea283df53c
Igor Petrov <Igor_Petrov@epam.com>
2020-02-19 16:10:13 +0400

    entry point for python client has been added

--- CodeBattlePython/snakebattleclient/Main.py
+++ CodeBattlePython/snakebattleclient/Main.py
@@ -18,3 +18,6 @@ def main():
     gcb = GameClient(
         "http://epruizhsa0001t2:8080/codenjoy-contest/board/player/ygzwtnd2yae0jn0y8viy?code=1104845394797194217&gameName=snakebattle")
     gcb.run(turn)
+
+if __name__ == '__main__':
+    main()
